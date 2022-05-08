8771ed1bac53fd0a287a1e7fbb06d36472509aa1
Celine19981 <celinedjeddiusthb@gmail.com>
2022-05-08 16:33:58 +0200

    uncommented export "C"

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,8 @@
 
 #include <iostream>
-#include "Composant1.h"
-#include "Composant2.h"
+#include <dlfcn.h>
+// #include "Composant1.h"
+// #include "Composant2.h"
 
 int main(int argc, char ** argv)
 {
@@ -10,11 +11,31 @@ int main(int argc, char ** argv)
 
 	int valeur1;
 	int valeur2;
+    int (*composant1)(int, int);
+    int (*composant2)(int, int);
+    char* (*getComposant1Version)(void);
 
-	valeur1=composant1(data1,data2);
+	void* comp1 = dlopen("libComposant1.so", RTLD_LAZY);
+	if (!comp1) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
 
-	valeur2=composant2(data1,data2);
+	void* comp2 = dlopen("libComposant2.so", RTLD_LAZY);
+	if (!comp2) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+	*(void **) (&composant1) = dlsym(comp1, "composant1");
+	*(void **) (&composant2) = dlsym(comp2, "composant2");
+    *(void **) (&getComposant1Version) = dlsym(comp1, "getComposant1Version");
 
-	std::cout << getComposant1Version() << std::endl;
-	std::cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2 << std::endl;
+	// valeur1=(*composant1)(data1,data2);
+	// valeur2=composant2(data1,data2);
+
+	std::cout << composant1 << std::endl;
+	std::cout << composant2 << std::endl;
+	std::cout << getComposant1Version << std::endl;
+	// std::cout << (*getComposant1Version)() << std::endl;
+	// std::cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2 << std::endl;
 }
